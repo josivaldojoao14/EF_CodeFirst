165483e606bd18b8606590d9401046a0ec38c613
Josivaldo <josivaldojoao14@gmail.com>
2021-02-15 00:13:53 -0300

    CRUD usando entity framework code first

---
+++ LearningCodeFirst/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningCodeFirst.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do usuário é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O sobrenome do usuário é obrigatório")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "O email do usuário é obrigatório")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
+        public string Email { get; set; }
+    }
+}
---
+++ LearningCodeFirst/obj/Debug/net5.0/5zjregcg.sql
@@ -0,0 +1,29 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Users] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NOT NULL,
+    [Sobrenome] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210215015347_InitialMigration', N'5.0.3');
+GO
+
+COMMIT;
+GO
+
